b3f6de033134224a02df5e9e6fc177add5002e39
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2023-12-12 00:42:24 -0300

    fix(poc1): fix firebase env config

--- src/modules/main/config/firebase.js
+++ src/modules/main/config/firebase.js
@@ -1,15 +1,8 @@
 const admin = require("firebase-admin");
-const fs = require("fs");
 
-const { FIREBASE_CONFIG_BASE_64 } = require("./vars");
+const firebaseAuthConfig = require("./firebaseAuthConfig");
 
 try {
-  const buffer = Buffer.from(FIREBASE_CONFIG_BASE_64, "base64");
-  fs.writeFileSync("./src/modules/main/config/firebaseAuthConfig.js", buffer);
-
-  // eslint-disable-next-line global-require, import/no-unresolved, import/extensions
-  const firebaseAuthConfig = require("./firebaseAuthConfig");
-
   admin.initializeApp({
     credential: admin.credential.cert(firebaseAuthConfig),
   });
