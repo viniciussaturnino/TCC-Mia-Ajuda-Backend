2fbbb8c6d17bc3d48b4085c270dc9b251ad8f7da
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2024-06-25 10:35:19 -0300

    fix(poc1): remove firebase config from project

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const { API_PORT } = require("./src/modules/main/config/vars");
 
 const databaseConnect = require("./src/modules/main/config/database");
 
-require("./src/modules/main/config/firebase");
-
 databaseConnect();
 
 app.listen(API_PORT, () =>
-  console.log(`Servidor rodando na porta ${API_PORT}!`),
+  console.log(`Servidor rodando na porta ${API_PORT}!`)
 );
 
 module.exports = app;
